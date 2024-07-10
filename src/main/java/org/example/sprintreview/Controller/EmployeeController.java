d02fcb5ccbe984b6300edf252e50400e1a3ede5b
Qausimodo75 <kwazikubheka777@gmail.com>
2024-07-10 14:54:01 +0200

    Controller api endepoints are working

--- src/main/java/org/example/sprintreview/Controller/EmployeeController.java
+++ src/main/java/org/example/sprintreview/Controller/EmployeeController.java
@@ -1,9 +1,39 @@
 package org.example.sprintreview.Controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.example.sprintreview.Entity.EmployeeInterface;
+import org.example.sprintreview.Model.Employee;
+import org.example.sprintreview.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
 
-public class EmployeeController {
-}
+    @RestController
+    public class EmployeeController implements EmployeeInterface {
+
+        @Autowired
+        private EmployeeService employeeService;
+
+        @GetMapping("/employees")
+        public List<Employee> getEmployees(){
+            return employeeService.getEmployees();
+        }
+
+        @GetMapping("/employees/{employeeId}")
+        public Employee getSingleEmployee(@PathVariable int employeeId){
+            return employeeService.getSingleEmployee(employeeId);
+        }
+
+        @PostMapping("/employees")
+        public Employee addNewEmployee(@RequestBody Employee employee){
+            return employeeService.addNewEmployee(employee);
+        }
+
+        @DeleteMapping("/employees/{employeeId}")
+        public List<Employee> deleteEmployee(@PathVariable int employeeId){
+            return employeeService.deleteEmployee(employeeId);
+        }
+
+    }
+
